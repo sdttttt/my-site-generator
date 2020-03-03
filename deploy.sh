328b94dc7c99861b014de884880fbb3e3672599c
SDTTTTT <760159537@qq.com>
2020-03-03 16:12:06 +0800

    [SDTTTTT] Update Blog.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,15 @@
-code_address="https://github.com/sdttttt/my-site-generator.git"
-deploy="https://github.com/sdttttt/Site.git"
+#!/bin/bash
+
+# Hugo 部署脚本 on Github Pages
+# 基本是通用的
+# 在用之前注意一些事项
+# TODO: Git config 中的全局用户名和邮箱已经配置完毕
+# TODO: Hugo 已经安装，在当前环境变量下可以使用
+# TODO: code_address 和 deploy 确保配置成自己的
+# Author: SDTTTTT
+
+code_address="https://github.com/sdttttt/my-site-generator.git" # Hugo 项目地址
+deploy="https://github.com/sdttttt/Site.git" # 静态网站部署地址
 
 dir=$(pwd)
 
@@ -7,46 +17,46 @@ function deployToSite(){
     cp -r ./public ../
     cd ../public
 
-    echo "==> [Deploy] Git Runing ..."
+    echo "==> [Deploy] Git Runing ...\n"
 
     git init
     git add .
-    git commit -m "Update Site ..."
-    git push $deploy master
+    git commit -m "[SDTTTTT] Update Site."
+    git push $deploy master --force
 
     cd ..
-    rm -rfv ./public
+    rm -rf ./public
 
     cd $dir
 
-    echo "==> OK Deploy Over :)"
+    echo "==> OK Deploy Over :) \n"
 }
 
-echo "==> [Code] Git Runing ..."
+echo "==> [Code] Git Runing ... \n"
 
 git add .
 git commit -m "[SDTTTTT] Update Blog."
 git push $code_address master
 
-echo "==> Hugo Building ..."
+echo "==> Hugo Building ... \n"
 
 hugo
 
-echo "==> Check Status ..."
+echo "==> Check Status ... \n"
 
 if [ $? -eq 0 ]; then
     if [ -d "./public" ]; then
-        echo "Check OK :)"
+        echo "Check OK :) \n"
         deployToSite
     else
-        echo "Oh! 不应该变成这样 :("
+        echo "Oh! 不应该变成这样 :( \n"
     fi
 else 
-    echo "环境变量中不存在 hugo: 请安装它"
+    echo "环境变量中不存在 hugo: 请安装它 \n"
 fi
 
-echo "==> Clean work start!"
+echo "==> Clean work start! \n"
 
 rm -rf ./public
 
-echo "==> OK! We is done."
+echo "==> OK! We is done. \n"
