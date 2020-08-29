98ac0ee1e09096a5e414d0b8fa19e95b4f3cf25f
sdttttt <760159537@qq.com>
2020-08-29 16:23:28 +0800

    [SDTTTTT] Update Blog.

--- deploy.sh
+++ deploy.sh
@@ -68,13 +68,13 @@ function syncSourceCode(){
     if [ -n  $code_address_gitee ];
     then
         echo -e "\033[32m[Synchronizing]\033[0m Source code to Gitee..."
-        git push -q $code_address_gitee master &
+        git push $code_address_gitee master &
         pid=$!
         echo -e "\033[32m[Synchronizing]\033[0m Source code to Github..."
-        git push -q $code_address master
+        git push $code_address master
         wait $pid
     else
-        git push -q $code_address master
+        git push $code_address master
     fi
 }
 
